dc5010595eec55ea25db7b91f4c7a1a552737f3e
Shamar <Shamar098@gmail.com>
2025-05-14 15:22:16 +0100

    build: initialise

---
+++ src/main/java/com/example/curator/ExhibitionCuratorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.curator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionCuratorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitionCuratorBackendApplication.class, args);
+	}
+
+}
