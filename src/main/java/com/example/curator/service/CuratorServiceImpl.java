2e3f3747ff332d0d4d0c0239c5582a90330b779c
Shamar <Shamar098@gmail.com>
2025-06-01 01:05:36 +0100

    feat: getArtworkSearchResults CuratorService method

--- src/main/java/com/example/curator/service/CuratorServiceImpl.java
+++ src/main/java/com/example/curator/service/CuratorServiceImpl.java
@@ -1,16 +1,15 @@
 package com.example.curator.service;
 
-import com.example.curator.model.Artwork;
+import com.example.curator.model.ArtworkResults;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 public class CuratorServiceImpl implements CuratorService{
     @Autowired
     ApiService apiService;
 
     @Override
-    public List<Artwork> getArtworkSearchResults(String query) {
-        return null;
+    public ArtworkResults getArtworkSearchResults(String query, Integer page) {
+        ArtworkResults artworkSearchResults = apiService.getArtworkSearchResults(query,page);
+        return artworkSearchResults;
     }
 }
