f29deb8776e16db288dddacddf676df70ece4a88
Shamar <Shamar098@gmail.com>
2025-06-02 04:02:53 +0100

    refactor: CollectionRepository to ExhibitionRepository

--- src/main/java/com/example/curator/repository/ExhibitionRepository.java
+++ src/main/java/com/example/curator/repository/ExhibitionRepository.java
@@ -3,5 +3,5 @@ package com.example.curator.repository;
 import com.example.curator.model.Exhibition;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CollectionRepository extends CrudRepository<Exhibition, Long> {
+public interface ExhibitionRepository extends CrudRepository<Exhibition, Long> {
 }
