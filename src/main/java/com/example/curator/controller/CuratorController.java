2a439fd2e0dd3a848c10ab1971302c7072e50dfd
Shamar <Shamar098@gmail.com>
2025-06-03 02:02:10 +0100

    feat: getApiArtworkDetailsById endpoint

--- src/main/java/com/example/curator/controller/CuratorController.java
+++ src/main/java/com/example/curator/controller/CuratorController.java
@@ -1,13 +1,13 @@
 package com.example.curator.controller;
 
+import com.example.curator.dto.ArtworkDTO;
 import com.example.curator.model.ArtworkResults;
 import com.example.curator.service.CuratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class CuratorController {
@@ -17,6 +17,11 @@ public class CuratorController {
     @GetMapping("/search")
     public ResponseEntity<ArtworkResults> getArtworkSearchResults(@RequestParam("query") String query, @RequestParam("page") Integer page){
     return new ResponseEntity<>(service.getArtworkSearchResults(query,page), HttpStatus.OK);
+    }
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<ArtworkDTO> getApiArtworkDetailsById(@PathVariable("id") Long id,@RequestParam("apiOrigin") String apiOrigin){
+        return new ResponseEntity<>(service.getApiArtworkDetails(id, apiOrigin),HttpStatus.OK);
     }
 
 }
