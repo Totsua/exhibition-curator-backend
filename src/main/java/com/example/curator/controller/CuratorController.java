d5a84671a989b4170a9df37887ae10db92c31c81
Shamar <Shamar098@gmail.com>
2025-06-03 03:35:23 +0100

    feat: createExhibition method

--- src/main/java/com/example/curator/controller/CuratorController.java
+++ src/main/java/com/example/curator/controller/CuratorController.java
@@ -1,6 +1,7 @@
 package com.example.curator.controller;
 
 import com.example.curator.dto.ArtworkDTO;
+import com.example.curator.dto.ExhibitionDTO;
 import com.example.curator.model.ArtworkResults;
 import com.example.curator.service.CuratorService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,11 @@ public class CuratorController {
     @GetMapping("/search/{id}")
     public ResponseEntity<ArtworkDTO> getApiArtworkDetailsById(@PathVariable("id") Long id,@RequestParam("apiOrigin") String apiOrigin){
         return new ResponseEntity<>(service.getApiArtworkDetails(id, apiOrigin),HttpStatus.OK);
+    }
+    // Create an exhibition
+    @PostMapping("/exhibitions/create")
+    public ResponseEntity<ExhibitionDTO> createExhibition(@RequestParam String title){
+        return new ResponseEntity<>(service.createExhibition(title),HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/example/curator/service/CuratorServiceImpl.java
+++ src/main/java/com/example/curator/service/CuratorServiceImpl.java
@@ -1,14 +1,23 @@
 package com.example.curator.service;
 
+import com.example.curator.dto.ArtistDTO;
 import com.example.curator.dto.ArtworkDTO;
+import com.example.curator.dto.ExhibitionDTO;
+import com.example.curator.model.Artwork;
 import com.example.curator.model.ArtworkResults;
+import com.example.curator.model.Exhibition;
+import com.example.curator.repository.ExhibitionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class CuratorServiceImpl implements CuratorService{
     @Autowired
     ApiService apiService;
+    @Autowired
+    ExhibitionRepository exhibitionRepository;
 
     @Override
     public ArtworkResults getArtworkSearchResults(String query, Integer page) {
@@ -21,5 +30,44 @@ public class CuratorServiceImpl implements CuratorService{
         ArtworkDTO artwork = apiService.getApiArtworkDetails(id, apiOrigin);
         // todo: catch custom artwork exception
         return artwork;
+    }
+
+    @Override
+    public ExhibitionDTO createExhibition(String title) {
+        if(exhibitionRepository.existsByTitle(title)){
+            System.out.println("Exhibition already exists with that title");
+            // todo: throw custom 'exhibition already exists' exception
+        }
+
+
+        Exhibition exhibition = Exhibition.builder().title(title).build();
+        Exhibition exhibitionDTO = exhibitionRepository.save(exhibition);
+        return exhibitionToDTOMapper(exhibitionDTO);
+    }
+
+    private ExhibitionDTO exhibitionToDTOMapper(Exhibition exhibition) {
+        ArrayList<ArtworkDTO> artworks = new ArrayList<>();
+        for(Artwork artwork: exhibition.getArtworks()){
+            artworks.add(artworkToDTO(artwork));
+        }
+
+        return ExhibitionDTO.builder()
+                .id(exhibition.getId())
+                .title(exhibition.getTitle())
+                .description(exhibition.getDescription())
+                .artworks(artworks)
+                .build();
+    }
+
+    private ArtworkDTO artworkToDTO(Artwork artwork){
+        return ArtworkDTO.builder()
+                .id(artwork.getApiId())
+                .title(artwork.getTitle())
+                .description(artwork.getDescription())
+                .altText(artwork.getAltText())
+                .apiOrigin(artwork.getApiOrigin())
+                .imageUrl(artwork.getImageUrl())
+                .artist(new ArtistDTO(artwork.getArtist().getApiId(),artwork.getArtist().getName()))
+                .build();
     }
 }
