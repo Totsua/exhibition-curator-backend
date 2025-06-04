3c067ccc00c7e24aac4510c58ca22a176c05a9ca
Shamar <Shamar098@gmail.com>
2025-06-04 23:59:54 +0100

    feat: add unique constraint to artist

--- src/main/java/com/example/curator/model/Artist.java
+++ src/main/java/com/example/curator/model/Artist.java
@@ -1,9 +1,6 @@
 package com.example.curator.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +11,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 @Data
+
+@Table(name = "artist", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"apiId", "apiOrigin"})
+})
 public class Artist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
