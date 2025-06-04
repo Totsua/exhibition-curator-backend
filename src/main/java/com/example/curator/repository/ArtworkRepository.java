75ed49d02b0f0392396fd323e6c090a813ed2fc1
Shamar <Shamar098@gmail.com>
2025-06-04 18:56:05 +0100

    feat: add ArtworkRepository methods

--- src/main/java/com/example/curator/repository/ArtworkRepository.java
+++ src/main/java/com/example/curator/repository/ArtworkRepository.java
@@ -3,5 +3,9 @@ package com.example.curator.repository;
 import com.example.curator.model.Artwork;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ArtworkRepository extends CrudRepository<Artwork,Long> {
+    Optional<Artwork> findByApiIdAndApiOrigin(Long apiId, String apiOrigin);
+    boolean existsByApiIdAndApiOrigin(Long apiId, String apiOrigin);
 }
