32e8e393bc42cd34ca833ec844632876b8ac6d09
Shamar <Shamar098@gmail.com>
2025-06-05 21:46:44 +0100

    feat: add page out of bounds exception

---
+++ src/main/java/com/example/curator/exception/APIPageOutOfBoundsException.java
@@ -0,0 +1,7 @@
+package com.example.curator.exception;
+
+public class APIPageOutOfBoundsException extends RuntimeException {
+    public APIPageOutOfBoundsException(String message) {
+        super(message);
+    }
+}
