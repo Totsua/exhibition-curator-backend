bc1978e98a40be877999fa27b28b8b320806778a
Shamar <Shamar098@gmail.com>
2025-06-05 05:28:18 +0100

    refactor: change annotation NotNull to NotBlank in ExhibitionCreateDTO

--- src/main/java/com/example/curator/dto/ExhibitionCreateDTO.java
+++ src/main/java/com/example/curator/dto/ExhibitionCreateDTO.java
@@ -1,6 +1,6 @@
 package com.example.curator.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -9,7 +9,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ExhibitionCreateDTO {
-    @NotNull(message = "Title cannot be null")
+    @NotBlank(message = "Title cannot be empty")
     @Size(min = 1, max = 80, message = "Title must be between 1 and 80 characters")
     private String title;
 
