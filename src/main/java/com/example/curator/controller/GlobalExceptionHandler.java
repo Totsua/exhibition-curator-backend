4eec2bafde34e72d2e461e58af45a36d68d45365
Shamar <Shamar098@gmail.com>
2025-06-09 03:20:28 +0100

    feat: handle duplicateArtworkException

--- src/main/java/com/example/curator/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/curator/controller/GlobalExceptionHandler.java
@@ -3,8 +3,15 @@ package com.example.curator.controller;
 import com.example.curator.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,7 +21,7 @@ public class GlobalExceptionHandler {
     }
     @ExceptionHandler(APIPageOutOfBoundsException.class)
     public ResponseEntity<Object> handleAPIPageOutOfBoundsException(APIPageOutOfBoundsException e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE); //todo: potentially 416 (Range Not Satisfied)
     }
     @ExceptionHandler(UnknownAPIOriginException.class)
     public ResponseEntity<Object> handleUnknownAPIOriginException(UnknownAPIOriginException e){
@@ -30,6 +37,25 @@ public class GlobalExceptionHandler {
     }
     @ExceptionHandler(InvalidExhibitionException.class)
     public ResponseEntity<Object> handleInvalidExhibitionException(InvalidExhibitionException e){
-        return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(DuplicateArtworkException.class)
+    public ResponseEntity<Object> handleDuplicateArtworkException(DuplicateArtworkException e){
+        return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(final MethodArgumentNotValidException ex) {
+
+        final BindingResult bindingResult = ex.getBindingResult();
+        final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        final Map<String, String> errors = new HashMap<>();
+        fieldErrors.forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+
+
 }
