70034bac155e88c7dc43014b08521c3190500368
Shamar <Shamar098@gmail.com>
2025-06-13 16:21:00 +0100

    feat: add description to ExhibitionCreateDTO

--- src/main/java/com/example/curator/dto/ExhibitionCreateDTO.java
+++ src/main/java/com/example/curator/dto/ExhibitionCreateDTO.java
@@ -13,4 +13,6 @@ public class ExhibitionCreateDTO {
     @Size(min = 1, max = 80, message = "Title must be between 1 and 80 characters")
     private String title;
 
+    @Size(max = 500, message = "Description must below 500 characters")
+    private String description;
 }
